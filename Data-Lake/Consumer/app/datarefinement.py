9934bb3d10b036d06796e094e2ff5c60db7bf6ff
RKChrist <roo12ney@gmail.com>
2022-06-11 08:18:21 +0200

    Sounds good

--- Data-Lake/Consumer/app/datarefinement.py
+++ Data-Lake/Consumer/app/datarefinement.py
@@ -6,54 +6,50 @@ import pandas as pd
 from nltk.corpus import stopwords
 import numpy as np 
 
-allstopwords = stopwords.words('english')
-sw_list = ['I','i', '.',"''","'",",","...","!","``","-","?","ll","|",":","'s","'f'","'wo'", "'se'","n't", "'m", "f","'ll", "'ne'", "se"]
-allstopwords.extend(sw_list)
+def ExtractKeyWords():
+    allstopwords = stopwords.words('english')
+    sw_list = ['I','i', '.',"''","'",",","...","!","``","-","?","ll","|",":","'s","'f'","'wo'", "'se'","n't", "'m", "f","'ll", "'ne'", "se"]
+    allstopwords.extend(sw_list)
 
-stopwords = set(allstopwords)
+    stopwords = set(allstopwords)
 
-def generator(x, count):
-    for pages in range(x-1):
-        pageobj = pdfreader.getPage((count))
-        yield nltk.word_tokenize(pageobj.extract_text())
-        count=count + 1
+    def generator(x, count):
+        for pages in range(x-1):
+            pageobj = pdfreader.getPage((count))
+            yield nltk.word_tokenize(pageobj.extract_text())
+            count=count + 1
 
-#download if run for the first time
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('stopwords')
+    #download if run for the first time
+    nltk.download('punkt')
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('stopwords')
 
-# Decode the Base64 string, making sure that it contains only valid characters
-text = base64.b64decode(base64.b64encode(b'data'), validate=True)
-# Find the path to the file
-base_path = Path(__file__).parent
-files_path = (base_path / "../../pdf/Percy Jackson & the Olympians 01 - The Lightning Thief.pdf").resolve()
+    # Decode the Base64 string, making sure that it contains only valid characters
+    text = base64.b64decode(base64.b64encode(b'data'), validate=True)
+    # Find the path to the file
+    base_path = Path(__file__).parent
+    files_path = (base_path / "../../pdf/Percy Jackson & the Olympians 01 - The Lightning Thief.pdf").resolve()
 
-pdffileobj=open(files_path,'rb')
+    pdffileobj=open(files_path,'rb')
 
-#create reader variable that will read the pdffileobj
-pdfreader=PyPDF2.PdfFileReader(pdffileobj)
+    #create reader variable that will read the pdffileobj
+    pdfreader=PyPDF2.PdfFileReader(pdffileobj)
 
-#This will store the number of pages of this pdf file
-x=pdfreader.numPages
-
-
-#create a variable that will select the selected number of pages
-pageobj = pdfreader.getPage(0)
-txt = pageobj.extractText()
-pagetoken = nltk.word_tokenize(txt)
-for d in generator(x, 1):
-    pagetoken.extend(d)
-
-filtered_token = []
-filtered_token.extend([w for w in pagetoken if not w.lower() in stopwords])
-
-print(type(filtered_token[0]))
-df = pd.DataFrame(filtered_token, columns =["text"])
-amountOfValue = df["text"].value_counts()
+    #This will store the number of pages of this pdf file
+    x=pdfreader.numPages
 
 
-    
-print(df["text"].drop_duplicates())
+    #create a variable that will select the selected number of pages
+    pageobj = pdfreader.getPage(0)
+    txt = pageobj.extractText()
+    pagetoken = nltk.word_tokenize(txt)
+    for d in generator(x, 1):
+        pagetoken.extend(d)
+
+    filtered_token = []
+    filtered_token.extend([w for w in pagetoken if not w.lower() in stopwords])
+
+def AnalyseKeywords(listOfKeywords: list[str]):
+    listOfKeywords
 
         
--- Data-Lake/javascript.js
+++ Data-Lake/javascript.js
@@ -1,6 +1,13 @@
+var { Kafka } = require(['kafkajs'])
 
+var  host = "kafka-server1"
 
+var  kafka = new Kafka({
+  brokers: ['kafka-server1:9098']
+})
 
+var  topic = 'pdf_topic'
+var producer = kafka.producer()
 
 function submitbutton1() 
 {
@@ -11,6 +18,10 @@ function submitbutton1()
   console.log(email);
   var phone=$("#phone").val();
   console.log(phone);
+
+  var today = new Date();
+
+  var createdDate = today.getDate() + '-' + (today.getMonth()+1)+'-'+  today.getFullYear();
 
   var filename = ($("#resume").val()).split(/(\\|\/)/g).pop()
   var blob = document.getElementById("resume").files[0];
@@ -23,37 +34,76 @@ function submitbutton1()
   
   
   
-  blobToBase64(blob).then(value => {
-    console.log(value);
-    var data = value;
-    viewer(data);});
   
+   var data = blobToBase64(blob).then(value => {
+    
+    var data = value;
+    return data; });
+
+
+
+    var json = "{userid=key, " +
+    " filename=" +  filename +", " +
+    " createddate=" + createdDate +" ," +
+    " data=" + data + ", " +
+    " filetype=" +  filetype + ", " +
+    " createdby=" + name + ", "
+
+
+
+    const produce = async () => {
+      try{
+        await producer.connect()
+      } catch (err) {
+        console.log(err)
+      }
+
+      let i = 0
+    
+      // after the produce has connected, we start an interval timer
+      setInterval(async () => {
+        try {
+          console.log("Hello")
+          // send a message to the configured topic with
+          // the key and value formed from the current value of `i`
+          await producer.send({
+            topic,
+            messages: [
+              {
+                key: String(i),
+                value: data,
+              },
+            ],
+          })
+    
+          // if the message is written successfully, log it and increment `i`
+          console.log("writes: ", i)
+          i++
+        } catch (err) {
+          console.error("could not write message " + err)
+        }
+      }, 1000)
+    
+    }
+    produce();
 }
-function viewer(data){
-    console.log(data);
-    new Promise(r => setTimeout(r, 100));
-    var arraybuf = base64ToArrayBuffer(data);
-    var blob = new Blob([arraybuf], {type: "application/pdf"});
-    console.log(blob);
-    var newelement = window.URL.createObjectURL(blob);
-    document.getElementById("WebGl").src = newelement;
-}
+
 function blobToBase64(blob) {
-    return new Promise((resolve, _) => {
-      const reader = new FileReader();
-      reader.onloadend = () => resolve(reader.result);
-      reader.readAsDataURL(blob);
-    });
-  }
+  return new Promise((resolve, _) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result);
+    reader.readAsDataURL(blob);
+  });
+}
 
 //This function takes a base64 string and adds it to a Uint8array, after that is done it should return the array
 function base64ToArrayBuffer(base64) {
-    var binaryString = atob(base64.split(",")[1]);
-    var binaryLen = binaryString.length;
-    var bytes = new Uint8Array(binaryLen);
-    for (var i = 0; i < binaryLen; i++) {
-        var ascii = binaryString.charCodeAt(i);
-        bytes[i] = ascii;
-    }
-    return bytes;
+  var binaryString = atob(base64.split(",")[1]);
+  var binaryLen = binaryString.length;
+  var bytes = new Uint8Array(binaryLen);
+  for (var i = 0; i < binaryLen; i++) {
+      var ascii = binaryString.charCodeAt(i);
+      bytes[i] = ascii;
+  }
+  return bytes;
 }
