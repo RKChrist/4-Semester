66222ca1343023bb339f46a200df54ae5240c5ad
RKChrist <roo12ney@gmail.com>
2022-06-17 06:12:15 +0200

    Commit

--- Data-Lake/Consumer/app/app.py
+++ Data-Lake/Consumer/app/app.py
@@ -3,78 +3,74 @@ from fastapi import FastAPI
 import json
 from pydantic import BaseModel
 from typing import Optional
-import datarefinement
+
 
 
 app = FastAPI()
 
-class jsonObject(BaseModel):
+class jsonObjectOut(BaseModel):
      filename: str
      createddate: str
-     data: str
+     data: Optional[bytes]
      filetype: str
-     version: str
+     version: int
      createdby: str
 
-class Item(BaseModel):
-     message: Optional[list[str]] = []
 
 
-@app.get('/kafka/', response_model=Item)
-async def get_messages():
-    item = Item()
-    print("FUCK")
-    result = []
-    newresult = kafkaConsumer(result)  
-    print("Woaws")
-    for message in newresult:
-        item.message.append(message.get('key').decode('utf-8') + ": username:" + message.get('username')+ ", " + "firstname:" + message.get('firstname') + ", lastname:" + message.get('lastname'))
-        print("Type")
-        print(type(item))
-        print("Another one")
+
+# @app.get('/kafka/', response_model=Item)
+# async def get_messages():
+#     item = Item()
+#     print("FUCK")
+#     result = []
+#     newresult = kafkaConsumer(result)  
+#     print("Woaws")
+#     for message in newresult:
+#         item.message.append(message.get('key').decode('utf-8') + ": username:" + message.get('username')+ ", " + "firstname:" + message.get('firstname') + ", lastname:" + message.get('lastname'))
+#         print("Type")
+#         print(type(item))
+#         print("Another one")
 
   
-    return item
+#     return item
+@app.get('/kafka/')
+async def get_messages():
+    result = []
+    newresult = kafkaConsumer(result)  
+    return newresult
+        
+
+  
+    return "Hello"
 
     
 def kafkaConsumer(result):
     # To consume latest messages and auto-commit offsets
     consumer = KafkaConsumer(bootstrap_servers=['kafka-server1:9092'], 
-                             value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+                            #  value_deserializer=lambda m: json.loads(m.decode('utf-8')),
                              sasl_mechanism="PLAIN",
                              auto_offset_reset='earliest'
                             )
-    
-    partitions = []
-    for partition in consumer.partitions_for_topic('json_topic'):
-        partitions.append(TopicPartition('json_topic', partition))
-    
-    end_offsets = consumer.end_offsets(partitions)
-    endofstream = end_offsets.get(partitions[0])
-    
-    
-    amount = endofstream
-    print("amount")
-    print(amount)
-    if(amount <= 10):
-        amount == 0
-    else:
-        amount = amount - 10
-    
 
-    par = TopicPartition(topic='json_topic', partition=0)
+
+    par = TopicPartition(topic='json_topic', partition=2)
     consumer.assign([par])
-    consumer.seek(par, amount) 
-    count = 0
-    for message in consumer: 
-        result.append({"key": message.key,
-                       "username":message.value.get('username'),
-                       "firstname" : message.value.get('firstname'), 
-                       "lastname" : message.value.get('lastname')})
-        count = count + 1
-        print(count)
-        if(count==10):
-            break
+    consumer.seek(par, 0) 
+    
+    for item in consumer: 
+        val = item.value.decode('utf-8')
+        result = val
+        break
+        # result.append({"filename": item.value.filename, 
+        #      "createddate": item.value.createddate, 
+        #      "data": item.value.data.decode("utf-8"), 
+        #      "filetype": item.value.filetype,
+        #      "version" : item.value.version,
+        #      "createdby":item.value.createdby})
+        # count = count + 1
+        # print(count)
+        
         
     print("works")
     return result
